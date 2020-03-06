600e928cdbc191d375add2b284bd131cd8519975
Zac <zacace@ingoglia.com>
2020-03-06 14:57:10 -0700

    EOD Day 3
    + Built Switcher JS somewhat

---
+++ scripts/menuContent.js
@@ -0,0 +1,88 @@
+var currentMenu = "";
+function appetizersMenu() {
+    currentMenu = '<div id="appetizersMealsBox" class="menuContainer">';
+    currentMenu += '<div id="menuTitleBox" class="centeredHorBox">';
+    currentMenu += '<h2 id="menuTitle">Appetizers</h2>';
+    currentMenu += '</div>';
+    currentMenu += '<div id="appetizersMeals" class="subSectionBox">';
+    currentMenu += '<div id="menuItems" class="leftHorBox">';
+    currentMenu += '<dl id="menuItemList">';
+    currentMenu += '<dt id="name" class="menuItem">name</dt>';
+    currentMenu += '<dd id="desc" class="menuItemDesc">description</dd>';
+
+    currentMenu += '<dt id="name" class="menuItem">name</dt>';
+    currentMenu += '<dd id="desc" class="menuItemDesc">description</dd>';
+
+    currentMenu += '<dt id="name" class="menuItem">name</dt>';
+    currentMenu += '<dd id="desc" class="menuItemDesc">description</dd>';
+
+    currentMenu += '<dt id="name" class="menuItem">name</dt>';
+    currentMenu += '<dd id="desc" class="menuItemDesc">description</dd>';
+
+    currentMenu += '<dt id="name" class="menuItem">name</dt>';
+    currentMenu += '<dd id="desc" class="menuItemDesc">description</dd>';
+
+    currentMenu += '<dt id="name" class="menuItem">name</dt>';
+    currentMenu += '<dd id="desc" class="menuItemDesc">description</dd>';
+
+    currentMenu += '<dt id="name" class="menuItem">name</dt>';
+    currentMenu += '<dd id="desc" class="menuItemDesc">description</dd>';
+
+    currentMenu += '<dt id="name" class="menuItem">name</dt>';
+    currentMenu += '<dd id="desc" class="menuItemDesc">description</dd>';
+
+    currentMenu += '<dt id="name" class="menuItem">name</dt>';
+    currentMenu += '<dd id="desc" class="menuItemDesc">description</dd>';
+
+    currentMenu += '<dt id="name" class="menuItem">name</dt>';
+    currentMenu += '<dd id="desc" class="menuItemDesc">description</dd>';
+
+    currentMenu += '<dt id="name" class="menuItem">name</dt>';
+    currentMenu += '<dd id="desc" class="menuItemDesc">description</dd>';
+
+    currentMenu += '<dt id="name" class="menuItem">name</dt>';
+    currentMenu += '<dd id="desc" class="menuItemDesc">description</dd>';
+
+    currentMenu += '<dt id="name" class="menuItem">name</dt>';
+    currentMenu += '<dd id="desc" class="menuItemDesc">description</dd>';
+
+    currentMenu += '<dt id="name" class="menuItem">name</dt>';
+    currentMenu += '<dd id="desc" class="menuItemDesc">description</dd>';
+
+    currentMenu += '<dt id="name" class="menuItem">name</dt>';
+    currentMenu += '<dd id="desc" class="menuItemDesc">description</dd>';
+
+    currentMenu += '<dt id="name" class="menuItem">name</dt>';
+    currentMenu += '<dd id="desc" class="menuItemDesc">description</dd>';
+
+    currentMenu += '<dt id="name" class="menuItem">name</dt>';
+    currentMenu += '<dd id="desc" class="menuItemDesc">description</dd>';
+    currentMenu += '</dl>';
+    currentMenu += '</div>';
+    currentMenu += '</div>';
+    currentMenu += '</div>';
+}
+function soupsMenu() {
+
+};
+function thaiMenu() {
+
+};
+function entreesMenu() {
+
+};
+function noodlesMenu() {
+
+};
+function veggiesMenu() {
+
+};
+function riceMenu() {
+
+};
+function dessertsMenu() {
+
+};
+function specialsMenu() {
+
+};
